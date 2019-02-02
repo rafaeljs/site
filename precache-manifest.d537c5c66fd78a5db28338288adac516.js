420c80cd1e573fa4d024d6c9a9f39a5a383195ee
rafael <rafaeljsilverio@hotmail.com>
2019-02-02 14:04:11 -0300

    Updates

---
+++ precache-manifest.d537c5c66fd78a5db28338288adac516.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dfc88b21976a5ef9fd4d7b5d71a44476",
+    "url": "/portfolio/static/media/Code.dfc88b21.jpg"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "996f451a100c97d8914c",
+    "url": "/portfolio/static/js/main.996f451a.chunk.js"
+  },
+  {
+    "revision": "024cfc33d9d93d7e8524",
+    "url": "/portfolio/static/js/1.024cfc33.chunk.js"
+  },
+  {
+    "revision": "996f451a100c97d8914c",
+    "url": "/portfolio/static/css/main.e09ffeeb.chunk.css"
+  },
+  {
+    "revision": "125d423399587fff05ad1b0108511948",
+    "url": "/portfolio/index.html"
+  }
+];
